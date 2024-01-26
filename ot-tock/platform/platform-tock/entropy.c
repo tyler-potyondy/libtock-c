35261f8bd05dd88c7456abab5665d0691c143f00
Samir Rashid <samirsgotmail@gmail.com>
2024-01-26 00:40:45 -0800

    entropy. readme

--- ot-tock/platform/platform-tock/entropy.c
+++ ot-tock/platform/platform-tock/entropy.c
@@ -2,7 +2,20 @@
 
 #include<openthread/platform/entropy.h>
 
+#include <rng.h>
+
 otError otPlatEntropyGet(uint8_t *aOutput, uint16_t aOutputLength){
-    // TODO 
-    return OT_ERROR_NOT_IMPLEMENTED;
+    if (aOutput == NULL) {
+        return OT_ERROR_INVALID_ARGS;
+    }
+    
+    // Synchronously fill buffer
+    int count;
+    int returnCode = rng_sync(aOutput, (uint32_t) aOutputLength, (uint32_t) aOutputLength, &count);
+
+    // TODO: test printing count in tock example app. The documentation doesn't make sense.
+    if (returnCode == RETURNCODE_SUCCESS) { // RETURNCODE_SUCCESS is 0
+        return OT_ERROR_NONE;
+    }
+    return OT_ERROR_FAILED;
 }
