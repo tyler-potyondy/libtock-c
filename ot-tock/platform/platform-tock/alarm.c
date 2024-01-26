ef5bc36a5dade40c7a1f7528eb124f7e1b4c159d
Samir Rashid <samirsgotmail@gmail.com>
2024-01-25 20:09:08 -0800

    Work on alarm

--- ot-tock/platform/platform-tock/alarm.c
+++ ot-tock/platform/platform-tock/alarm.c
@@ -2,35 +2,50 @@
 
 #include<openthread/platform/alarm-micro.h>
 #include<openthread/platform/alarm-milli.h>
-void otPlatAlarmMicroStartAt(otInstance *aInstance, uint32_t aT0, uint32_t aDt){
-    printf("alarm micro start\n");
-    // TODO
-}
 
-void otPlatAlarmMicroStop(otInstance *aInstance){
-    printf("alarm micro stop\n");
-    // TODO
-}
+// TODO: need to keep track of timers on my own
+// in a static var, and have a callback which
+// to tell OS to unblock?
 
-uint32_t otPlatAlarmMicroGetNow(void){
-    printf("alarm micro get now\n");
-    // TODO
-    return 0;
-}
+// TODO: #include <libtock/alarm.h>
+
+// NOTE: Micro is not necessary
+
+// void otPlatAlarmMicroStartAt(otInstance *aInstance, uint32_t aT0, uint32_t aDt){
+//     printf("alarm micro start\n");
+//     static alarm_t alarm;
+//     alarm_at(aT0, aDt, aInstance, void*(0), &alarm); // TODO: check callback
+// }
+
+// void otPlatAlarmMicroStop(otInstance *aInstance){
+//     printf("alarm micro stop\n");
+//     alarm_cancel(aInstance);
+// }
+
+// uint32_t otPlatAlarmMicroGetNow(void){
+//     printf("alarm micro get now\n");
+//     // TODO
+//     return 0;
+// }
 
 void otPlatAlarmMilliStartAt(otInstance *aInstance, uint32_t aT0, uint32_t aDt){
     printf("alarm milli start\n");
     // TODO
+    static alarm_t alarm;
+    alarm_at(aT0, aDt, aInstance, void*(0), &alarm); // TODO: check callback
 }
 
 void otPlatAlarmMilliStop(otInstance *aInstance){
     printf("alarm milli stop\n");
-    // TODO
+    alarm_cancel(aInstance);
 }
 
 uint32_t otPlatAlarmMilliGetNow(void){
     printf("alarm milli get now\n");
-    // TODO
-    return 0;
+    struct timeval tv;
+    gettimeasticks(&tv, NULL); // second arg is unused
+    
+    nowMicro = tv->tv_usec;
+    return nowMicro / 1000;
 }
 
