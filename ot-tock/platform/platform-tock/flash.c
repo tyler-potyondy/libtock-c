fb8ba2307e45322b44c5094a90a55cb713e24ac3
Samir Rashid <samirsgotmail@gmail.com>
2024-01-27 19:43:02 -0800

    100% debug print coverage

--- ot-tock/platform/platform-tock/flash.c
+++ ot-tock/platform/platform-tock/flash.c
@@ -3,25 +3,30 @@
 #include<openthread/platform/flash.h>
 #include <internal/nonvolatile_storage.h>
 
+#include <stdio.h>
 // Why did @Tyler not include this?
 // uint32_t otPlatFlashGetSwapSize(otInstance *aInstance);
 
 void otPlatFlashInit(otInstance *aInstance) {
     // TODO
     OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
 }
 
 void otPlatFlashErase(otInstance *aInstance, uint8_t aSwapIndex) {
     // TODO
     OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
 }
 
 void otPlatFlashWrite(otInstance *aInstance, uint8_t aSwapIndex, uint32_t aOffset, const void *aData, uint32_t aSize) {
     // TODO
     OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
 }
 
 void otPlatFlashRead(otInstance *aInstance, uint8_t aSwapIndex, uint32_t aOffset, void *aData, uint32_t aSize) {
     // TODO
     OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
 }
