90a88418ca110ec3be1b45d301c6ca8512c23947
Samir Rashid <samirsgotmail@gmail.com>
2024-01-27 18:54:33 -0800

    mark unused var, fix printf

--- ot-tock/platform/platform-tock/radio.c
+++ ot-tock/platform/platform-tock/radio.c
@@ -9,160 +9,188 @@ static otRadioCaps tempRadioCaps;
 
 void otPlatRadioGetIeeeEui64(otInstance *aInstance, uint8_t *aIeeeEui64) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
 }
 
 void otPlatRadioSetPanId(otInstance *aInstance, uint16_t aPanid) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
 }
 
 void otPlatRadioSetExtendedAddress(otInstance *aInstance, const otExtAddress *aExtAddress) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
 }
 
 void otPlatRadioSetShortAddress(otInstance *aInstance, uint16_t aShortAddress) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
 }
 
 bool otPlatRadioIsEnabled(otInstance *aInstance) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return true;
 }
 
 otError otPlatRadioEnable(otInstance *aInstance) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 otError otPlatRadioDisable(otInstance *aInstance) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 otError otPlatRadioSleep(otInstance *aInstance) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 otError otPlatRadioReceive(otInstance *aInstance, uint8_t aChannel) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 otError otPlatRadioTransmit(otInstance *aInstance, otRadioFrame *aFrame) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 otRadioFrame *otPlatRadioGetTransmitBuffer(otInstance *aInstance) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return &tempFrame;
 }
 
 int8_t otPlatRadioGetRssi(otInstance *aInstance) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return 0;
 }
 
 otRadioCaps otPlatRadioGetCaps(otInstance *aInstance) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return tempRadioCaps;
 }
 
 bool otPlatRadioGetPromiscuous(otInstance *aInstance) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return true;
 }
 
 void otPlatRadioSetPromiscuous(otInstance *aInstance, bool aEnable) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
 }
 
 void otPlatRadioEnableSrcMatch(otInstance *aInstance, bool aEnable) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
 }
 
 otError otPlatRadioAddSrcMatchShortEntry(otInstance *aInstance, uint16_t aShortAddress) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 otError otPlatRadioAddSrcMatchExtEntry(otInstance *aInstance, const otExtAddress *aExtAddress) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 otError otPlatRadioClearSrcMatchShortEntry(otInstance *aInstance, uint16_t aShortAddress) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 otError otPlatRadioClearSrcMatchExtEntry(otInstance *aInstance, const otExtAddress *aExtAddress) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 void otPlatRadioClearSrcMatchShortEntries(otInstance *aInstance) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
 }
 
 void otPlatRadioClearSrcMatchExtEntries(otInstance *aInstance) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
 }
 
 otError otPlatRadioEnergyScan(otInstance *aInstance, uint8_t aScanChannel, uint16_t aScanDuration) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 otError otPlatRadioGetTransmitPower(otInstance *aInstance, int8_t *aPower) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 otError otPlatRadioSetTransmitPower(otInstance *aInstance, int8_t aPower) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 otError otPlatRadioGetCcaEnergyDetectThreshold(otInstance *aInstance, int8_t *aThreshold) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 otError otPlatRadioSetCcaEnergyDetectThreshold(otInstance *aInstance, int8_t aThreshold) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return OT_ERROR_NOT_IMPLEMENTED;
 }
 
 int8_t otPlatRadioGetReceiveSensitivity(otInstance *aInstance) {
     // TODO
-    printf("%s:%s in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
     return 0;
 }
--- ot-tock/platform/platform-tock/settings.c
+++ ot-tock/platform/platform-tock/settings.c
@@ -1,34 +1,44 @@
-#include<openthread/platform/settings.h>
+#include <openthread/platform/settings.h>
 
-void otPlatSettingsInit(otInstance *aInstance, const uint16_t *aSensitiveKeys, uint16_t aSensitiveKeysLength){
-	// TODO
+void otPlatSettingsInit(otInstance *aInstance, const uint16_t *aSensitiveKeys,
+                        uint16_t aSensitiveKeysLength) {
+    // TODO
+    OT_UNUSED_VARIABLE(aInstance);
 }
 
-
-void otPlatSettingsDeinit(otInstance *aInstance){
-	// TODO
+void otPlatSettingsDeinit(otInstance *aInstance) {
+    // TODO
+    OT_UNUSED_VARIABLE(aInstance);
 }
 
-otError otPlatSettingsGet(otInstance *aInstance, uint16_t aKey, int aIndex, uint8_t *aValue, uint16_t *aValueLength){
-	// TODO
-	return OT_ERROR_NOT_IMPLEMENTED;
+otError otPlatSettingsGet(otInstance *aInstance, uint16_t aKey, int aIndex, uint8_t *aValue,
+                          uint16_t *aValueLength) {
+    // TODO
+    OT_UNUSED_VARIABLE(aInstance);
+    return OT_ERROR_NOT_IMPLEMENTED;
 }
 
-otError otPlatSettingsSet(otInstance *aInstance, uint16_t aKey, const uint8_t *aValue, uint16_t aValueLength){
-	// TODO
-	return OT_ERROR_NOT_IMPLEMENTED;
+otError otPlatSettingsSet(otInstance *aInstance, uint16_t aKey, const uint8_t *aValue,
+                          uint16_t aValueLength) {
+    // TODO
+    OT_UNUSED_VARIABLE(aInstance);
+    return OT_ERROR_NOT_IMPLEMENTED;
 }
 
-otError otPlatSettingsAdd(otInstance *aInstance, uint16_t aKey, const uint8_t *aValue, uint16_t aValueLength){
-	// TODO
-	return OT_ERROR_NOT_IMPLEMENTED;
+otError otPlatSettingsAdd(otInstance *aInstance, uint16_t aKey, const uint8_t *aValue,
+                          uint16_t aValueLength) {
+    // TODO
+    OT_UNUSED_VARIABLE(aInstance);
+    return OT_ERROR_NOT_IMPLEMENTED;
 }
 
-otError otPlatSettingsDelete(otInstance *aInstance, uint16_t aKey, int aIndex){
-	// TODO
-	return OT_ERROR_NOT_IMPLEMENTED;
+otError otPlatSettingsDelete(otInstance *aInstance, uint16_t aKey, int aIndex) {
+    // TODO
+    OT_UNUSED_VARIABLE(aInstance);
+    return OT_ERROR_NOT_IMPLEMENTED;
 }
 
-void otPlatSettingsWipe(otInstance *aInstance){
-	// TODO
+void otPlatSettingsWipe(otInstance *aInstance) {
+    // TODO
+    OT_UNUSED_VARIABLE(aInstance);
 }
