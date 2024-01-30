978342ff932e0c4c2818a1d38a637bb9cb3a411c
Samir Rashid <samirsgotmail@gmail.com>
2024-01-30 10:36:24 -0800

    copy micro alarm implementation

--- ot-tock/platform/platform-tock/alarm.c
+++ ot-tock/platform/platform-tock/alarm.c
@@ -8,6 +8,7 @@
 
 // Based on reference implementations (https://github.com/openthread/ot-nxp/blob/main/src/common/alarm_freertos.c and https://github.com/openthread/ot-nrf528xx/blob/main/src/src/alarm.c), it seems there should only be one timer.
 static alarm_t alarm;
+static alarm_t alarm_micro;
 
 
 static void callback(int now, int interval, int arg2, void *aInstance) {
@@ -22,7 +23,7 @@ void otPlatAlarmMilliStartAt(otInstance *aInstance, uint32_t aT0, uint32_t aDt){
     printf("alarm: setting alarm %lums after %lums. It's currently %lu\n", aDt, aT0,
            otPlatAlarmMilliGetNow());
     // aDt = 10;
-    int return_code = alarm_at(aT0, aDt, callback, (void *)aInstance, &alarm);  // TODO: check callback
+    int return_code = alarm_at(aT0, aDt, callback, (void *)aInstance, &alarm);
                        // this needs to be in clock units, not ms
 
     printf("alarm returncode %d\n", return_code);
@@ -47,3 +48,35 @@ uint32_t otPlatAlarmMilliGetNow(void) {
     return nowMilli32bit;
     // return 0;
 }
+
+// Symmetric implementation for microsecond alarm
+// WARNING: no prints when this code is included?
+/*
+void otPlatAlarmMicroStartAt(otInstance *aInstance, uint32_t aT0, uint32_t aDt){
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("alarm micro start\n");
+    printf("alarm_micro: setting alarm %lums after %lums. It's currently %lu\n", aDt, aT0,
+           otPlatAlarmMilliGetNow());
+    int return_code = alarm_at(aT0, aDt, callback, (void *)aInstance, &alarm_micro);
+    printf("alarm_micro returncode %d\n", return_code);
+}
+
+void otPlatAlarmMicroStop(otInstance *aInstance) {
+    OT_UNUSED_VARIABLE(aInstance);
+    printf("alarm_micro stop\n");
+    alarm_cancel(&alarm_micro);
+}
+
+uint32_t otPlatAlarmMicroGetNow(void) {
+    printf("alarm_micro get now: ");
+    struct timeval tv;
+    gettimeasticks(&tv, NULL);  // second arg is unused
+    
+    uint32_t nowSeconds = tv.tv_sec;
+    uint32_t nowMicro = tv.tv_usec;
+    uint32_t nowMicro32bit = (nowSeconds * 1000 * 1000) + (nowMicro);
+
+    printf("%lu\n", nowMicro32bit);
+    return nowMicro32bit;
+}
+*/
