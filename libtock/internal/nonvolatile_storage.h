9e29dc1258071cac3ad271c6df252f84454cdb40
Samir Rashid <samirsgotmail@gmail.com>
2024-02-07 18:38:55 -0800

    Add nonvolatile storage comments

--- libtock/internal/nonvolatile_storage.h
+++ libtock/internal/nonvolatile_storage.h
@@ -8,14 +8,35 @@
 extern "C" {
 #endif
 
+/*
+* Set a callback to be called after reads.
+*/
 int nonvolatile_storage_internal_read_done_subscribe(subscribe_upcall cb, void *userdata);
+/*
+* Set a callback to be called after writes.
+*/
 int nonvolatile_storage_internal_write_done_subscribe(subscribe_upcall cb, void *userdata);
 
+/*
+* Set the buffer for read data will be read from.
+*/
 int nonvolatile_storage_internal_read_buffer(uint8_t* buffer, uint32_t len);
+/*
+* Set the buffer for writes to output to.
+*/
 int nonvolatile_storage_internal_write_buffer(uint8_t* buffer, uint32_t len);
 
+/*
+* Get the size of the flash.
+*/
 int nonvolatile_storage_internal_get_number_bytes(int* number_bytes);
+/*
+* Get the size of the flash.
+*/
 int nonvolatile_storage_internal_read(uint32_t offset, uint32_t length);
+/*
+* Get the size of the flash.
+*/
 int nonvolatile_storage_internal_write(uint32_t offset, uint32_t length);
 
 #ifdef __cplusplus
