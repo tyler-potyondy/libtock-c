50a04c78c6a665fe487f2f1c8ed4e8de08e80a10
Samir Rashid <samirsgotmail@gmail.com>
2024-02-07 19:02:33 -0800

    System platform file done

--- ot-tock/platform/platform-tock/system.c
+++ ot-tock/platform/platform-tock/system.c
@@ -1,20 +1,26 @@
 // TODO COPYRIGHT
 #include <openthread-system.h>
 #include <stdio.h>
+
 void otSysInit(int argc, char *argv[]) {
     // TODO
-    
-    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
+    OT_UNUSED_VARIABLE(argc);
+    OT_UNUSED_VARIABLE(argv);
+    printf("%s:%d in %s\n", __FILE__, __LINE__, __func__);
 }
 
+void otSysDeinit(void) {
+    // TODO: Need to check if there is functionality for reloading drivers/hardware
+    printf("%s:%d in %s\n", __FILE__, __LINE__, __func__);
+}
 bool otSysPseudoResetWasRequested(void) {
     // TODO
-    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
+    printf("%s:%d in %s\n", __FILE__, __LINE__, __func__);
     return false;
 }
 
 void otSysProcessDrivers(otInstance *aInstance) {
-    // TODO
-    printf("%s:%d in %s", __FILE__, __LINE__, __func__);
+    // This function gets called a lot by the main loop.
+    // We do not have to call into any drivers as Tock will handle it.
     OT_UNUSED_VARIABLE(aInstance);
 }
