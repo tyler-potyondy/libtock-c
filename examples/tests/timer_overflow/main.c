fa393c7fc1ec873b03dbedbde47974c3b9c7e205
Anthony Tarbinian <atar137h@gmail.com>
2024-04-08 23:25:35 -0700

    remove busy waits, replace with yield
    Apparently, timer callbacks won't be scheduled if our process is just
    busy waiting. We though the kernel would preempt the process to schedule
    the callback, but we have learned it does not. Instead, the process must
    give up the CPU to the kernel to allow the callback to get run.
    
    Also for some reason we need a print statement after calling yield.
    Otherwise the callback does not get scheduled.
    
    Co-Authored-By: Samir Rashid <samir-rashid@users.noreply.github.com>

--- examples/tests/timer_overflow/main.c
+++ examples/tests/timer_overflow/main.c
@@ -19,8 +19,6 @@
 
 #define TIMER_LEN_MS 2000
 
-#define SECONDISH_BUSY_WAIT_TIME 10000000
-
 // helper struct to send data to callback
 typedef struct testing_ud {
     uint32_t timer_start_ticks;
@@ -32,26 +30,11 @@ static void timer_fire_callback(int                        last_timer_fire_ticks
                               __attribute__ ((unused)) int unused1,
                               __attribute__ ((unused)) int unused2,
                               void*                        ud) {
-    // uint32_t now;
-    // int ret = alarm_internal_read(&now);
-    // printf("%ld %d\n", now, last_timer_fire_ticks);
-    
     testing_ud_t* test_ud = (testing_ud_t*)ud; 
-    printf("Expected timer to fire at %ld ticks\n", test_ud->expected_timer_end_ticks);
-    printf("Actually fire at %d ticks\n", last_timer_fire_ticks);
-    printf("Difference of %ld ticks\n", test_ud->expected_timer_end_ticks - last_timer_fire_ticks);
-}
-
-// we wanted busy wait because calling `delay_ms` uses `timer_in`, which
-// is what we are trying to test. it was triggering our debug statements.
-volatile uint32_t x = 0;
-static void busy_wait(uint32_t num) {
-    uint32_t loop = 0;
-    while (loop < num) {
-        loop++;
-        // modifying a volatile variable x to avoid the loop being optimized out
-        x = loop;
-    }
+    printf("Timer Fired!\n");
+    printf("\tExpected timer to fire at %ld ticks\n", test_ud->expected_timer_end_ticks);
+    printf("\tActually fired at %d ticks\n", last_timer_fire_ticks);
+    printf("\tDifference of %ld ticks\n", test_ud->expected_timer_end_ticks - last_timer_fire_ticks);
 }
 
 inline static uint32_t ms_to_ticks(uint32_t ms) {
@@ -66,8 +49,6 @@ inline static uint32_t ms_to_ticks(uint32_t ms) {
 
 int main(int argc, char** argv)
 {
-    busy_wait(SECONDISH_BUSY_WAIT_TIME);
-
     // need to allocate memory for a timer before giving a pointer to `timer_in`
     tock_timer_t* timer = malloc(sizeof(tock_timer_t));
     
@@ -78,9 +59,12 @@ int main(int argc, char** argv)
     ud->timer_start_ticks = now_ticks;
     ud->expected_timer_end_ticks = now_ticks + ms_to_ticks(TIMER_LEN_MS);
 
+    printf("Setting timer to fire at %ld ticks\n", ud->expected_timer_end_ticks);
     timer_in(TIMER_LEN_MS, timer_fire_callback, (void*)ud, timer);
 
-    // need to busy wait for a bit longer so the program doesn't exit before 
-    // the timer fires
-    busy_wait(SECONDISH_BUSY_WAIT_TIME);
+    // yield back to the kernel because otherwise timer callback won't be scheduled
+    yield();
+    // TODO: For some reason this print is needed. Othwerwise the timer callback never
+    // gets scheduled even with the yield in the previous line
+    printf(":(\n");
 }
