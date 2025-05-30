c1f7cad1f381107ead7ac0b98212f83733872e73
Tyler Potyondy <potyondyt@gmail.com>
2025-05-30 12:14:20 -0700

    [openthread] update flash impl to use isolated nv

--- libopenthread/platform/flash.c
+++ libopenthread/platform/flash.c
@@ -1,5 +1,5 @@
-#include <storage/nonvolatile_storage.h>
-#include <libtock-sync/storage/nonvolatile_storage.h>
+#include <storage/isolated_nonvolatile_storage.h>
+#include <libtock-sync/storage/isolated_nonvolatile_storage.h>
 
 #include <openthread/platform/flash.h>
 
@@ -15,19 +15,26 @@ uint32_t otPlatFlashGetSwapSize(otInstance *aInstance){
 
 void otPlatFlashInit(otInstance *aInstance) {
     OT_UNUSED_VARIABLE(aInstance);
+
+    // Confirm we have enough space for the 2 swaps.
+    uint64_t num_bytes;
+    libtocksync_isolated_nonvolatile_storage_get_number_bytes(&num_bytes);
+
+    assert(num_bytes >= SWAP_SIZE * SWAP_NUM);
+    
 }
 
 void otPlatFlashErase(otInstance *aInstance, uint8_t aSwapIndex) {
     OT_UNUSED_VARIABLE(aInstance);
+    OT_UNUSED_VARIABLE(aSwapIndex);
+    
+    uint8_t overwrite[SWAP_SIZE];
+    // Fill the swap area with 0xFF to erase it.
+    for (uint32_t i = 0; i < SWAP_SIZE; i++) {
+        overwrite[i] = 0xFF;
+    }
 
-    uint32_t num_bytes;
-    libtock_nonvolatile_storage_get_number_bytes(&num_bytes);
-
-    // Write all zeroes to flash
-    uint8_t zeroes[SWAP_SIZE];
-
-    otPlatFlashWrite(aInstance, aSwapIndex, 0, zeroes, num_bytes);
-
+    otPlatFlashWrite(aInstance, aSwapIndex, 0, overwrite, SWAP_SIZE);
 }
 
 void otPlatFlashWrite(otInstance *aInstance, uint8_t aSwapIndex, uint32_t aOffset,
@@ -40,8 +47,7 @@ void otPlatFlashWrite(otInstance *aInstance, uint8_t aSwapIndex, uint32_t aOffse
     uint32_t offset = aSwapIndex ? SWAP_SIZE : 0;
     offset += aOffset;
 
-    int _written;
-    ret = libtocksync_nonvolatile_storage_write(offset, aSize, (uint8_t*)aData, aSize, &_written);
+    ret = libtocksync_isolated_nonvolatile_storage_write(offset, (uint8_t*)aData, aSize);
     if (ret != RETURNCODE_SUCCESS) {
         return;
     }
@@ -52,13 +58,11 @@ void otPlatFlashRead(otInstance *aInstance, uint8_t aSwapIndex, uint32_t aOffset
     OT_UNUSED_VARIABLE(aInstance);
     OT_UNUSED_VARIABLE(aSwapIndex);
     int ret;
-
     assert(aSwapIndex < SWAP_NUM);
     uint32_t offset = aSwapIndex ? SWAP_SIZE : 0;
     offset += aOffset;
 
-    int _read;
-    ret = libtocksync_nonvolatile_storage_read(offset, aSize, (uint8_t*)aData, aSize, &_read);
+    ret = libtocksync_isolated_nonvolatile_storage_read(offset, (uint8_t*)aData, aSize);
     if (ret != RETURNCODE_SUCCESS) {
         return;
     }
